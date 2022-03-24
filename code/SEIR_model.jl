4963250720f67be5819c04e8c1d6aa6a4d125306
blanchette31 <brandonblanchette.30@gmail.com>
2022-03-24 11:23:33 -0400

    fixed equation D(A)

--- code/SEIR_model.jl
+++ code/SEIR_model.jl
@@ -21,9 +21,9 @@ using CSV: CSV
 D = Differential(t)
 
 # Equations 
-equations = [
+equations =  [
     D(S) ~ -β*S*A - β*S*Ifq*I - e*S,
-    D(A) ~ fq*I + s  ,
+    D(A) ~ β*S*A + β*S*I - fs*A - fr*A - s*ft*A + e*S,
     D(I) ~ fs*A - fr*I - fq*I,
     D(Q) ~ fq*I + s*ft*a - fr*Q,
     D(R) ~ fr*Q + fr*I + fr*A
