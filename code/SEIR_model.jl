fb294ef7833d9497d9d49e29c8964bd7f4e0a54c
blanchette31 <brandonblanchette.30@gmail.com>
2022-03-29 11:32:22 -0400

    formatted document

--- code/SEIR_model.jl
+++ code/SEIR_model.jl
@@ -1,7 +1,7 @@
 # Simulations
 using DifferentialEquations
 using OrdinaryDiffEq
-using ModelingToolkit 
+using ModelingToolkit
 
 # Figures
 using Plots
@@ -20,26 +20,34 @@ using CSV: CSV
 D = Differential(t)
 
 # Equations 
-equations =  [
-    D(S) ~ -β*S*A - β*S*I - e*S,
-    D(A) ~ β*S*A + β*S*I - fs*A - fr*A - s*ft*A + e*S,
-    D(I) ~ fs*A - fr*I - fq*I,
-    D(Q) ~ fq*I + s*ft*a - fr*Q,
-    D(R) ~ fr*Q + fr*I + fr*A
+equations = [
+    D(S) ~ -β * S * A - β * S * I - e * S,
+    D(A) ~ β * S * A + β * S * I - fs * A - fr * A - s * ft * A + e * S,
+    D(I) ~ fs * A - fr * I - fq * I,
+    D(Q) ~ fq * I + s * ft * a - fr * Q,
+    D(R) ~ fr * Q + fr * I + fr * A,
 ]
 
 # Problem 
 @named seir = ODESystem(equations)
-u0 = [S => 9999 , A => 1 , I => 0, R => 0, Q => 0]
-tspan = (0., 100.)
+u0 = [S => 9999, A => 1, I => 0, R => 0, Q => 0]
+tspan = (0.0, 100.0)
 
 # Simulations
 for r0 in [2.5, 3.8, 4.0, 5.0, 6.0, 6.5, 8.0]
     # TODO formula for parameters from R0
-    p = [ β => r0*fr/(S+A+I+R+Q), e => 4/10000 , fs => 0.6, fr => 1/14 , s => 0.9, ft => 2/7 , fq => 0.9]
+    p = [
+        β => r0 * fr / (S + A + I + R + Q),
+        e => 4 / 10000,
+        fs => 0.6,
+        fr => 1 / 14,
+        s => 0.9,
+        ft => 2 / 7,
+        fq => 0.9,
+    ]
     problem = ODEProblem(seir, u0, tspan, p)
     solution = solve(problem)
-    plot(solution; dpi=600, frame=:box)
+    plot(solution; dpi = 600, frame = :box)
     savefig(joinpath("figures", "simulation_$(r0).png"))
     # TODO save data to a CSV file
 end
