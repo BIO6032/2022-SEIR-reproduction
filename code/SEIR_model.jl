1d9a1c7da23d17432aabe42f7b92ee9fae7905eb
blanchette31 <brandonblanchette.30@gmail.com>
2022-03-29 11:29:04 -0400

    formula to determine beta

--- code/SEIR_model.jl
+++ code/SEIR_model.jl
@@ -36,10 +36,13 @@ tspan = (0., 100.)
 # Simulations
 for r0 in [2.5, 3.8, 4.0, 5.0, 6.0, 6.5, 8.0]
     # TODO formula for parameters from R0
-    p = [ β => b, e => 0.00004 , fs => 0.6, fr => 1/14 , s => 0.9, ft => 2/7 , fq => 0.9]
+    p = [ β => b, e => 4/10000 , fs => 0.6, fr => 1/14 , s => 0.9, ft => 2/7 , fq => 0.9]
     problem = ODEProblem(seir, u0, tspan, p)
     solution = solve(problem)
     plot(solution; dpi=600, frame=:box)
     savefig(joinpath("figures", "simulation_$(r0).png"))
     # TODO save data to a CSV file
 end
+
+
+b = r0/(14*10000) # to add to for loop
