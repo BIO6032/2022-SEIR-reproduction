c060e56e21547200d09ef05533f42dd03c16464a
blanchette31 <brandonblanchette.30@gmail.com>
2022-03-29 11:46:14 -0400

    corrected a for A

--- code/SEIR_model.jl
+++ code/SEIR_model.jl
@@ -8,7 +8,7 @@ using Plots
 
 # Data
 using DataFrames
-using CSV: CSV
+#using CSV:CSV
 
 # Parameters 
 @parameters t β e fs fr s ft fq
@@ -24,7 +24,7 @@ equations = [
     D(S) ~ -β * S * A - β * S * I - e * S,
     D(A) ~ β * S * A + β * S * I - fs * A - fr * A - s * ft * A + e * S,
     D(I) ~ fs * A - fr * I - fq * I,
-    D(Q) ~ fq * I + s * ft * a - fr * Q,
+    D(Q) ~ fq * I + s * ft * A - fr * Q,
     D(R) ~ fr * Q + fr * I + fr * A,
 ]
 
