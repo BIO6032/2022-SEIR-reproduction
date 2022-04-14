a890935525f02eb26524ecac57f3494cbfdeb5f3
Salomé Bouskila <82335200+salomebsk@users.noreply.github.com>
2022-04-14 11:45:50 -0400

    Added Comment + nettoyage

--- code/SEIR_model.jl
+++ code/SEIR_model.jl
@@ -1,7 +1,9 @@
 # Simulations
 using DifferentialEquations
 using OrdinaryDiffEq
+#Permet de résoudre des équations différentielles 
 using ModelingToolkit
+#Permet de résoudre de créer des problèmes et génèrer des solutions à partir des équations imputées au modèle
 
 # Figures
 using Plots
@@ -13,39 +15,82 @@ using CSV:CSV
 # Parameters 
 @parameters t β e fs fr s ft fq
 
-# Variables
+#Les paramètres sont les valeurs qui ne changent pas au cours du temps, ce sont des taux.
+#Beta = Taux de transmission 
+#e = taux exogène 
+#fs = taux de conversion de asymptomatique en symptomatique
+#ft = taux de dépistage
+#fr = taux auquel les individus deviennent recovered
+#s = sensibilité du test de dépistage 
+#fq = taux auquel les infectés symptomatiques et asymptomatiques sont placés en quarantaine
+
+# Variables 
 @variables S(t) A(t) I(t) R(t) Q(t) 
+
+# S(t) représente le compartiment des individus susceptibles sur le campus. 
+# A(t) répresente le compartiment des individus infectieux et asymptomatiques.
+# I(t) représente le compartiment des individus infectieux et symptomatiques.
+# R(t) répresente le compartiment des individus qui ont récupéré de l'infection du virus.
+# Q(t) représente le compartiment des individus qui ont été placées en quarantaine. 
 
 # Differential
 D = Differential(t)
 
-# Equations 
+# Les équations démontrent des variables qui changent selon le temps.
+# Equations
 equations = [
+    # les susceptibles reçoivent un apport B*S*A de transmission des asymptomatiques 
+    # moins B*S*I perdues vers les infectueux et e*S d'exogenou
     D(S) ~ -β * S * A - β * S * I - e * S,
+    # les asymptotiques reçoivent un taux B*S*A des symptomatiques moins le taux symptomatique perdues
+    # aux infectueux, un testing rate perdues aux Quarantine et un recovery rate fr perdues aux Recovery   
     D(A) ~ β * S * A + β * S * I - fs * A - fr * A - s * ft * A + e * S,
+    # les infectueux reçoivent un taux symptomatiques fs des asymptomatiques et perdent un testing rate fr et un recovery rate fq
     D(I) ~ fs * A - fr * I - fq * I,
+    # les quarantined reçoivent      
     D(Q) ~ fq * I + s * ft * A - fr * Q,
     D(R) ~ fr * Q + fr * I + fr * A
 ]
 
 # Problem 
+# On créé un système d'équations qui regroupe les dérivées pour chaque compartiments
 @named seir = ODESystem(equations)
+# On définit les valeurs initiales pour chaque variables.
 S0 = 9999.
 A0 = 1.
 I0 = 0.
 R0 = 0.
 Q0 = 0.
+# On assigne à chaque compartiment/variable sa valeur initiale dans le vecteur u0.
 u0 = [S => S0, A => A0, I => I0, R => R0, Q => Q0]
+# On définit l'intervalle de temps (0 à 100 pas de temps)
 tspan = (0.0, 100.0)
 
 
 # Simulations
+# On génère une fenêtre de plot ne présentant que des axes ("vide")
 plot()
-for r0 in 2:8
+r0_values = collect(LinRange(0.1, 2.0, 5))
+r_eff = similar(r0_values)
+c_eff = similar(r0_values)
+
+# On génère un range de valeurs pour r0 allant de 0.1 à 2 (ou autres) et prenant 5 valeurs (ou autres).
+# Fonction similar permet de créer un tableau de valeurs pour un ou plusieurs éléments donnés (ici, le R0), 
+# permet ainsi d'obtenir les valeurs de R eff et C eff pour les valeurs données de R0 
+
+#La boucle qui suit a pour fonction d'effectuer les simulations du modèle définit par les équation seir à partir de différentes 
+#valeurs de R_0. Cette boucle commence par définir les valeurs de R_0 qui seront utilisées pour effectuer les simulations. Ensuite, 
+# les valeurs des paramètres sont définies et les paramètres sont associées à leurs différentes valeurs. 
+# On définit et résout le système d'équations seir et on assigne les valeurs obtenus pour les différents compartiments à travers le temps.
+# Finalement, on trace le graphique des résultats des simluations pour les différentes valeurs de R_0 (nombre de cas positifs en fonction du temps)
+
+for (i,r0) in enumerate(r0_values) 
+# La boucle utilise i iteration (partant de 1) et r0 prend successivemnet la ith valeurs de r0_values (enumerate)
+
     # TODO formula for parameters from r0
     #r0 = 8.
 
-    # Get Parameter values
+    # Get Parameter values - on définit les valeurs des paramètres
     _fr = 1. / 14.
     #_β = r0 * _fr / _N
     _N = S0+A0+I0+R0+Q0
@@ -58,7 +103,7 @@ for r0 in 2:8
     _fq = 0.9
     _β = (r0*(_fs+_fr+_s*_ft))/_N
 
-
+# On associe les paramètres aux valeurs que nous avons déterminés
     p = [
         fr => _fr,
         β => _β,
@@ -69,13 +114,19 @@ for r0 in 2:8
         ft => _ft,
         fq => _fq
     ]
-    problem = ODEProblem(seir, u0, tspan, p)
+    
+    #Définit le problème qu'on veut résoudre en fonction du système d'équations, les valeurs initiales des paramètres, la durée des simulations
+    # ainsi que les paramètres. 
+    problem = ODEProblem(seir, u0, tspan, p) 
+   
+    # Résout le problème définit à la ligne précédente
     solution = solve(problem)
   
-    M = solution[1:end,:] # Matrix of results 5 compartments x 31 time steps
+    M = solution[1:end,:] # Matrix of results 5 compartments x 31 time steps - crée une matrice qui va être utilisée pour stocker les valeurs obtenus
+    # des variables au cours du temps
     
-    # Assign values to each compartment over time
-    S = solution[1,:]
+    # Assign values to each compartment over time - stockage des valeurs des variables dans chaque compartiment à partir de la solution
+    S = solution[1,:] # Dans l'objet solution prendre le 1er élément à tous les pas de temps
     A = solution[2,:]
     I = solution[3,:]
     Q = solution[4,:]
@@ -83,58 +134,37 @@ for r0 in 2:8
     
     #C = cumsum((_Ss + _s*_ft*_Tr) * (A/_Tr))
     #plot!(solution.t,C, lab=r0)
-    vecN = repeat([_N], outer=length(S))
-    C_alt = vecN-S 
-    plot!(solution.t,C_alt, lab=r0)
-xaxis!(tspan...)
+    vecN = repeat([_N], outer=length(S)) # crée un vecteur N qui a la même longueur que le vecteur du compartiment S et répète la valeur de N 
+    # afin de pouvoir faire le calcul qui suit
+    
+    C_alt = vecN-S # formule qui calcule le nombre de cas positifs en fonction du temps
+    plot!(solution.t,C_alt, lab="", line_z=r0, clim=(0.0, 2.0), c=:roma) # plot des différentes
+
+    r_eff[i] = r0 / (1 + _Ss + _s * _ft * _Tr)
+
+    c_eff[i] = cumsum((_Ss + _s*_ft*_Tr) * (A/_Tr))
+
+
 end
-# Try with different R0 values --> Figure 2A
+xaxis!(tspan...)
 
 
+# Figure 2B
+_NE₀ = 4.
+for _r₀ = collect(LinRange(1.0, 8.0, 100))
 
-
-
-
-#Figure 2B 
-# Calculate Reff
-#TODO: replace with parameters
-
-@parameters i
-# Parameters
-#_r₀ = [2:8]
-#for i in _r₀
-
-
-# Parameter values
-
- _Ss = 0.6 
- _Tr = 14. 
- _s = 0.9
- _ft = (2. /7.)*(1. /_Tr)
- _NE₀ = 4.
-
-
-# Formula Reff
-#Reff[i] = _r₀[i] / (1 + _Ss + _s * _ft * _Tr)
-
-_r₀ = collect(LinRange(1.0, 8.0, 100))
 Reff = _r₀ ./ (1 + _Ss + _s * _ft * _Tr)
 A = (Reff./(1.0.-Reff)).*(_NE₀*_Tr./_r₀)
-
-plot(_r₀ , A)
 C = cumsum((_Ss + _s*_ft*_Tr) * (A/_Tr))
-plot(Reff, C)
+
+plot(Reff , C)
+plot(Reff, C, xlims = (0.0,1.0))
+
+end 
+
+# Try with different R0 values --> Figure 2A
 
 # Find the R0 used for the numerical solution(eq 1-5)
 
 
-# Expression 10 
-#A = (Reff[i]/1-Reff[i])*(_NE₀*_Tr/_r₀[i])
-#A = ((NE₀*Tr)/(1-Reff))*1/(1+Ss+s*ft*Tr)
 
-#C = cumsum((_Ss + _s*_ft*_Tr) * (A/_Tr))
-#plot(Reff,C, lab=_r₀)
-
-
-
-end
