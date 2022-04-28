b44d958a9ebd9f8314c1333b98862d373207dfb3
Salomé Bouskila <82335200+salomebsk@users.noreply.github.com>
2022-04-28 10:05:01 -0400

    formatage rapide

--- code/SEIR_model.jl
+++ code/SEIR_model.jl
@@ -1,22 +1,17 @@
 # Simulations
-using DifferentialEquations 
+using DifferentialEquations
 using OrdinaryDiffEq
 #Permet de résoudre des équations différentielles 
 using ModelingToolkit
 #Permet de résoudre de créer des problèmes et génèrer des solutions à partir des équations imputées au modèle
-
 # Figures
 using Plots
-
 # Data
 using DataFrames
 using CSV
 
-
-
 # Parameters 
 @parameters t β e fs fr s ft fq
-
 #Les paramètres sont les valeurs qui ne changent pas au cours du temps, ce sont des taux.
 #Beta = Taux de transmission 
 #e = taux exogène 
@@ -27,8 +22,7 @@ using CSV
 #fq = taux auquel les infectés symptomatiques et asymptomatiques sont placés en quarantaine
 
 # Variables 
-@variables S(t) A(t) I(t) R(t) Q(t) 
-
+@variables S(t) A(t) I(t) R(t) Q(t)
 # S(t) représente le compartiment des individus susceptibles sur le campus. 
 # A(t) répresente le compartiment des individus infectieux et asymptomatiques.
 # I(t) représente le compartiment des individus infectieux et symptomatiques.
@@ -37,7 +31,6 @@ using CSV
 
 # Differential
 D = Differential(t)
-
 # Les équations démontrent des variables qui changent selon le temps.
 # Equations
 equations = [
@@ -53,23 +46,22 @@ equations = [
     # fq vers les quarantinedd      
     D(Q) ~ fq * I + s * ft * A - fr * Q,
     # les recovered reçoivent un recover rate des quarantined, des infectueux et des asymptotiques
-    D(R) ~ fr * Q + fr * I + fr * A
+    D(R) ~ fr * Q + fr * I + fr * A,
 ]
 
 # Problem 
 # On créé un système d'équations qui regroupe les dérivées pour chaque compartiments
 @named seir = ODESystem(equations)
 # On définit les valeurs initiales pour chaque variables.
-S0 = 9999.
-A0 = 1.
-I0 = 0.
-R0 = 0.
-Q0 = 0.
+S0 = 9999.0
+A0 = 1.0
+I0 = 0.0
+R0 = 0.0
+Q0 = 0.0
 # On assigne à chaque compartiment/variable sa valeur initiale dans le vecteur u0.
 u0 = [S => S0, A => A0, I => I0, R => R0, Q => Q0]
 # On définit l'intervalle de temps (0 à 100 pas de temps)
 tspan = (0.0, 100.0)
-
 
 # Simulations
 # On génère une fenêtre de plot ne présentant que des axes ("vide")
@@ -77,7 +69,6 @@ plot()
 r0_values = collect(LinRange(0.1, 8.0, 200))
 r_eff = similar(r0_values)
 c_eff = similar(r0_values)
-
 # On génère un range de valeurs pour r0 allant de 0.1 à 2 (ou autres) et prenant 5 valeurs (ou autres).
 # Fonction similar permet de créer un tableau de valeurs pour un ou plusieurs éléments donnés (ici, le R0), 
 # permet ainsi d'obtenir les valeurs de R eff et C eff pour les valeurs données de R0 
@@ -87,68 +78,63 @@ c_eff = similar(r0_values)
 # les valeurs des paramètres sont définies et les paramètres sont associées à leurs différentes valeurs. 
 # On définit et résout le système d'équations seir et on assigne les valeurs obtenus pour les différents compartiments à travers le temps.
 # Finalement, on trace le graphique des résultats des simluations pour les différentes valeurs de R_0 (nombre de cas positifs en fonction du temps)
+for (i, r0) in enumerate(r0_values)
+    # La boucle utilise i iteration (partant de 1) et r0 prend successivemnet la ith valeurs de r0_values (enumerate)
 
-for (i,r0) in enumerate(r0_values) 
-# La boucle utilise i iteration (partant de 1) et r0 prend successivemnet la ith valeurs de r0_values (enumerate)
-
-    # TODO formula for parameters from r0
-    #r0 = 8.
+    # TODO formula for parameters from r0 -> unavailable formula and unable to recover it
 
     # Get Parameter values - on définit les valeurs des paramètres
-    _fr = 1. / 14.
-    #_β = r0 * _fr / _N
-    _N = S0+A0+I0+R0+Q0
-    _e = 4. / _N 
-    _Ss = 0.6 
-    _Tr = 14. 
+    _fr = 1.0 / 14.0
+    _N = S0 + A0 + I0 + R0 + Q0
+    _e = 4.0 / _N
+    _Ss = 0.6
+    _Tr = 14.0
     _fs = _Ss * _fr
     _s = 0.9
-    _ft = (2. /7.)
+    _ft = (2.0 / 7.0)
     _fq = 0.9
-    _β = (r0*(_fs+_fr+_s*_ft))/_N
+    #_β = r0 * _fr / _N
+    _β = (r0 * (_fs + _fr + _s * _ft)) / _N
 
-# On associe les paramètres aux valeurs que nous avons déterminés
+    # On associe les paramètres aux valeurs que nous avons déterminés
     p = [
         fr => _fr,
         β => _β,
         e => _e,
-    
         fs => _fs,
         s => _s,
         ft => _ft,
-        fq => _fq
+        fq => _fq,
     ]
-    
-    #Définit le problème qu'on veut résoudre en fonction du système d'équations, les valeurs initiales des paramètres, la durée des simulations
+
+    # Définit le problème qu'on veut résoudre en fonction du système d'équations, les valeurs initiales des paramètres, la durée des simulations
     # ainsi que les paramètres. 
-    problem = ODEProblem(seir, u0, tspan, p) 
-   
+    problem = ODEProblem(seir, u0, tspan, p)
+
     # Résout le problème définit à la ligne précédente
     solution = solve(problem)
-  
-    M = solution[1:end,:] # Matrix of results 5 compartments x 31 time steps - crée une matrice qui va être utilisée pour stocker les valeurs obtenus
-    # des variables au cours du temps
-    
+    # Matrix of results 5 compartments * x time steps; crée une matrice qui va être utilisée pour stocker les valeurs obtenus des variables au cours du temps
+    M = solution[1:end, :] 
     # Assign values to each compartment over time - stockage des valeurs des variables dans chaque compartiment à partir de la solution
-    S = solution[1,:] # Dans l'objet solution prendre le 1er élément à tous les pas de temps
-    A = solution[2,:]
-    I = solution[3,:]
-    Q = solution[4,:]
-    R = solution[5,:]
-    
+    S = solution[1, :] # Dans l'objet solution prendre le 1er élément à tous les pas de temps
+    A = solution[2, :]
+    I = solution[3, :]
+    Q = solution[4, :]
+    R = solution[5, :]
+
     #C = cumsum((_Ss + _s*_ft*_Tr) * (A/_Tr))
     #plot!(solution.t,C, lab=r0)
-    vecN = repeat([_N], outer=length(S)) # crée un vecteur N qui a la même longueur que le vecteur du compartiment S et répète la valeur de N 
+    vecN = repeat([_N], outer = length(S)) # crée un vecteur N qui a la même longueur que le vecteur du compartiment S et répète la valeur de N 
     # afin de pouvoir faire le calcul qui suit
-    
-    C_alt = vecN-S # formule qui calcule le nombre de cas positifs en fonction du temps
-    plot!(solution.t,C_alt, lab="", line_z=r0, clim=(0.0, 2.0), c=:roma) # plot des différentes
+
+    C_alt = vecN - S # formule qui calcule le nombre de cas positifs en fonction du temps
+    plot!(solution.t, C_alt, lab = "", line_z = r0, clim = (0.0, 2.0), c = :roma) # plot des différentes
 
     maxA = maximum(A)
 
     r_eff[i] = r0 / (1 + _Ss + _s * _ft * _Tr)
 
-    c_eff[i] = (_Ss + _s*_ft*_Tr) * (maxA/_Tr)
+    c_eff[i] = (_Ss + _s * _ft * _Tr) * (maxA / _Tr)
 
 end
 xaxis!(tspan...)
@@ -161,118 +147,122 @@ R₀ = LinRange(0.1, 8.0, 200)
 x = fill(NaN, length(R₀))
 y = fill(NaN, length(R₀))
 
-for (i,_r₀) in enumerate(R₀)
-
+for (i, _r₀) in enumerate(R₀)
     # paramètres
-    _NE₀ = 4.
+    _NE₀ = 4.0
     _Ss = 0.6
     _s = 0.9
-    _ft = (2. /7.)
-    _Tr = 14.
-
+    _ft = (2.0 / 7.0)
+    _Tr = 14.0
 
     Reff = _r₀ ./ (1 + _Ss + _s * _ft * _Tr)
-    A = (Reff./(1.0.-Reff)).*(_NE₀*_Tr./_r₀)
+    A = (Reff ./ (1.0 .- Reff)) .* (_NE₀ * _Tr ./ _r₀)
 
+    C = (_Ss + _s * _ft * _Tr) * (A / _Tr)
 
-    C = (_Ss + _s*_ft*_Tr) * (A/_Tr)
-
-    if C < 0 
+    if C < 0
         break
     end
 
     x[i] = Reff
     y[i] = C
 
-end 
+end
 
 _last_valid_idx = findfirst(isnan, y)
 _last_valid_idx = isnothing(_last_valid_idx) ? length(y) : _last_valid_idx
 x = x[1:_last_valid_idx]
 y = y[1:_last_valid_idx]
 
-plot(x,y, legend=:bottomright, frame=:box, c=:red, lab="Asymptotic expression")
-plot!(r_eff,c_eff, c=:blue, lab="Numerical solution")
+plot(x, y, legend = :bottomright, frame = :box, c = :red, lab = "Asymptotic expression")
+plot!(r_eff, c_eff, c = :blue, lab = "Numerical solution")
 # Try with different R0 values --> Figure 2A
-
-# Find the R0 used for the numerical solution(eq 1-5)
+# Find the R0 used for the numerical solution(eq 1-5) -> unavailable formula and unable to recover it
 
 
 ### Figure 3
 
 # Importer les données
-
 df = DataFrame(CSV.File(joinpath("data", "suffolk_county_data.csv")))
 
 #Figure 3A pour les cas cumulatifs 
-plot(df[:,1], df[:,5], frame=:box, lab="cummulative cases", 
-xaxis="date", yaxis="Positive cases", color=:black, linestyle=:solid)
+plot(
+    df[:, 1],
+    df[:, 5],
+    frame = :box,
+    lab = "cummulative cases",
+    xaxis = "date",
+    yaxis = "Positive cases",
+    color = :black,
+    linestyle = :solid,
+)
 
 #Figures 3B pour les daily cases 
-plot(df[:,1], df[:,3], frame=:box, lab="daily cases", 
-xaxis="date", yaxis="Daily cases", color=:black, linestyle=:dash)
+plot(
+    df[:, 1],
+    df[:, 3],
+    frame = :box,
+    lab = "daily cases",
+    xaxis = "date",
+    yaxis = "Daily cases",
+    color = :black,
+    linestyle = :dash,
+)
 
 # ecrire equation #7 + p(t) + R contact tracing pour 3b
-
-P₀
-δ
-
-#p = P₀ * δ * (t-tp)
-
+#p = P₀ * δ * (t-tp) mais on considère que p=1 ici
 
 # paramètres
-_NE₀ = 4.
+_NE₀ = 4.0
 _Ss = 0.6
 _s = 0.9
-_ft = (2. /7.)
-_Tr = 14.
-_p = 1.
+_ft = (2.0 / 7.0)
+_Tr = 14.0
+_p = 1.0
 _r₀ = 2.5
 
 Reff = _r₀ ./ (1 + _Ss + _s * _ft * _Tr)
-A = (Reff./(1.0.-Reff)).*(_NE₀*_Tr./_r₀)
+A = (Reff ./ (1.0 .- Reff)) .* (_NE₀ * _Tr ./ _r₀)
 
-D(A) ~ _r₀ /_Tr * (1- (1/Reff))* A + _NE₀
+D(A) ~ _r₀ / _Tr * (1 - (1 / Reff)) * A + _NE₀
 
-
-
-### New system
+### Système alternatif avec D(A) = equation 7
 
 @parameters t β e fs fr s ft fq r₀ Tr Reff NE₀ Ss
-@variables S(t) A(t) I(t) R(t) Q(t) 
+@variables S(t) A(t) I(t) R(t) Q(t)
 
 D = Differential(t)
 
 equations_alt = [
     D(S) ~ -β * S * A - β * S * I - e * S,
-    D(A) ~ r₀ / Tr * (1- (1/Reff))* A + NE₀,
+    D(A) ~ r₀ / Tr * (1 - (1 / Reff)) * A + NE₀,
     D(I) ~ fs * A - fr * I - fq * I,
     D(Q) ~ fq * I + s * ft * A - fr * Q,
-    D(R) ~ fr * Q + fr * I + fr * A
+    D(R) ~ fr * Q + fr * I + fr * A,
 ]
 
 @named seir_alt = ODESystem(equations_alt)
-S0 = 9999.
-A0 = 1.
-I0 = 0.
-R0 = 0.
-Q0 = 0.
+S0 = 9999.0
+A0 = 1.0
+I0 = 0.0
+R0 = 0.0
+Q0 = 0.0
 u0 = [S => S0, A => A0, I => I0, R => R0, Q => Q0]
 
-
-_fr = 1. / 14.
-_N = S0+A0+I0+R0+Q0
-_e = 4. / _N 
-_Ss = 0.6 
-_Tr = 14. 
+## avec r₀=2.5
+_fr = 1.0 / 14.0
+_N = S0 + A0 + I0 + R0 + Q0
+_e = 4.0 / _N
+_Ss = 0.6
+_Tr = 14.0
 _fs = _Ss * _fr
 _s = 0.9
-_ft = (2. /7.)
+_ft = (2.0 / 7.0)
 _fq = 0.9
 _r₀ = 2.5
-_β = (_r₀*(_fs+_fr+_s*_ft))/_N
+_β = (_r₀ * (_fs + _fr + _s * _ft)) / _N
 _Reff = _r₀ ./ (1 + _Ss + _s * _ft * _Tr)
-_NE₀ = 4.
+_NE₀ = 4.0
 
 p_alt = [
     fr => _fr,
@@ -286,22 +276,19 @@ p_alt = [
     r₀ => _r₀,
     NE₀ => _NE₀,
     Tr => _Tr,
-    Reff => _Reff
+    Reff => _Reff,
 ]
 
-problem_alt = ODEProblem(seir_alt, u0, tspan, p_alt) 
-   
+problem_alt = ODEProblem(seir_alt, u0, tspan, p_alt)
+
 # Résout le problème définit à la ligne précédente
 solution_alt = solve(problem_alt)
 
-M = solution_alt[1:end,:] # Matrix of results 5 compartments x 31 time steps - crée une matrice qui va être utilisée pour stocker les valeurs obtenus
-# des variables au cours du temps
-
 # Assign values to each compartment over time - stockage des valeurs des variables dans chaque compartiment à partir de la solution
-S = solution_alt[1,:] # Dans l'objet solution prendre le 1er élément à tous les pas de temps
-A = solution_alt[2,:]
-I = solution_alt[3,:]
-Q = solution_alt[4,:]
-R = solution_alt[5,:]
+S = solution_alt[1, :] # Dans l'objet solution prendre le 1er élément à tous les pas de temps
+A = solution_alt[2, :]
+I = solution_alt[3, :]
+Q = solution_alt[4, :]
+R = solution_alt[5, :]
 
 plot(solution_alt.t, A)
